0efeb681e9adaa25411a50f9e6b9638978c9acb3
Marissa Nolan <ritz1484@cs.com>
2021-01-15 22:06:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = ["I", "am", "really", "learning"]
-  my_array.unshift("wow!")
+  my_array2 = ["I", "am", "really", "learning"]
+  my_array2.unshift("wow!")
 end
 
 def remove_element_from_end_of_array(array)
