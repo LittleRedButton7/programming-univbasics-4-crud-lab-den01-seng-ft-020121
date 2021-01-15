de5aae82c42a23153b99d7cadbe5c0fbf8ef44dc
Marissa Nolan <ritz1484@cs.com>
2021-01-15 22:10:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,6 +19,7 @@ end
 def remove_element_from_end_of_array(array)
   my_array3 = ["I", "am", "really", "learning", "arrays!"]
   rem_el_array = my_array3.shift
+  p rem_el_array
 end
 
 def remove_element_from_start_of_array(array)
